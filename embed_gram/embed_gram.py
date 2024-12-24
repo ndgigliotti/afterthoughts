fb99d3f406d960a0460f373595d6e43a78283954
Nicholas Gigliotti <ndgigliotti@gmail.com>
2024-12-24 00:04:46 -0500

    Add `encode_extract`

--- embed_gram/embed_gram.py
+++ embed_gram/embed_gram.py
@@ -16,15 +16,19 @@ class TokenizedDataset(Dataset):
     def __getitem__(self, idx):
         return {k: v[idx].squeeze(0) for k, v in self.inputs.items()}
 
+
 def get_ngram_idx(input_ids, ngram_range=(3, 6)):
     if isinstance(input_ids, torch.Tensor):
         input_ids = input_ids.cpu().numpy()
     ngram_idx = []
     for ngram_size in range(ngram_range[0], ngram_range[1] + 1):
-        idx = np.vstack([np.arange(input_ids.shape[1]) + i for i in range(ngram_size)]).T
+        idx = np.vstack(
+            [np.arange(input_ids.shape[1]) + i for i in range(ngram_size)]
+        ).T
         idx = idx[(idx[:, -1] < input_ids.shape[1])]
         ngram_idx.append(idx)
     return ngram_idx
+
 
 class NgramEncoder:
     def __init__(self, model_name, device="cuda"):
@@ -48,8 +52,10 @@ class NgramEncoder:
         ngrams = [y for x in ngrams for y in x]
         ngram_vecs = np.vstack(ngram_vecs)
         return ngrams, ngram_vecs
-    
-    def encode(self, docs, max_length=512, batch_size=32, amp=True, amp_dtype=torch.bfloat16):
+
+    def encode(
+        self, docs, max_length=512, batch_size=32, amp=True, amp_dtype=torch.bfloat16
+    ):
         if max_length is None:
             if self.tokenizer.model_max_length is None:
                 raise ValueError(
@@ -57,13 +63,44 @@ class NgramEncoder:
                 )
             max_length = self.tokenizer.model_max_length
             print(f"max_length set to {max_length}")
-        inputs = self.tokenizer(docs, max_length=max_length, padding="longest", truncation=True, return_tensors="pt")
-        loader = DataLoader(TokenizedDataset(inputs), batch_size=batch_size, shuffle=False, pin_memory=True)
-        outputs = []
+        inputs = self.tokenizer(
+            docs,
+            max_length=max_length,
+            padding="longest",
+            truncation=True,
+            return_tensors="pt",
+        )
+        loader = DataLoader(
+            TokenizedDataset(inputs),
+            batch_size=batch_size,
+            shuffle=False,
+            pin_memory=True,
+        )
+        token_embeds = []
         with torch.no_grad():
             with torch.cuda.amp.autocast(enabled=amp, dtype=amp_dtype):
                 for batch in loader:
-                    batch = {k: v.to(self.device, non_blocking=True) for k, v in batch.items()}
-                    outputs.append(self.model(**batch).last_hidden_state.cpu().numpy())
-        outputs = np.concatenate(outputs, axis=0)
-        return self.extract_ngrams(inputs["input_ids"], outputs)
+                    batch = {
+                        k: v.to(self.device, non_blocking=True)
+                        for k, v in batch.items()
+                    }
+                    token_embeds.append(
+                        self.model(**batch).last_hidden_state.cpu().numpy()
+                    )
+        token_embeds = np.concatenate(token_embeds, axis=0)
+        return inputs, token_embeds
+
+    def encode_extract(
+        self,
+        docs,
+        max_length=512,
+        batch_size=32,
+        ngram_range=(3, 6),
+        amp=True,
+        amp_dtype=torch.bfloat16,
+    ):
+        inputs, token_embeds = self.encode(docs, max_length, batch_size, amp, amp_dtype)
+        ngrams, ngram_vecs = self.extract_ngrams(
+            inputs["input_ids"], token_embeds, ngram_range
+        )
+        return ngrams, ngram_vecs
